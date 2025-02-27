23daa1cb3b3503c5b6198418b2d7eaa85d36d17c
jonowpRMIT <S4151053@student.rmit.edu.au>
2025-02-27 14:50:00 +1100

    Add files via upload

---
+++ addnumbers.java
@@ -0,0 +1,16 @@
+public class addnumbers {
+    
+    public static void main(String[] args) {
+
+
+        int num1, num2, num3, total;
+
+        num1 = 10;
+        num2 = 15;
+        num3 = 20;
+
+        total = num1 + num2 + num3;
+
+        System.out.println("sum of " + num1 + ", " + num2 + ", " + num3 + " is " + total);
+    }
+}
