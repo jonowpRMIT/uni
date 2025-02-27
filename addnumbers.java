7c9d339130684274b90d893c04ac06419a22ab7e
Jonathon <s4151053@student.rmit.com.au>
2025-02-27 14:52:49 +1100

    update

--- addnumbers.java
+++ addnumbers.java
@@ -6,7 +6,7 @@ public class addnumbers {
         int num1, num2, num3, total;
 
         num1 = 10;
-        num2 = 15;
+        num2 = 14;
         num3 = 20;
 
         total = num1 + num2 + num3;
